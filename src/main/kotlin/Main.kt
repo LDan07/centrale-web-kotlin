7f64f7ec5d0adda35ee591c6d75a3a2c106b8b19
Lucie Danard <lucie.danard@gmail.com>
2021-10-13 09:50:57 +0200

    ajout de fonctionnalitées

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,15 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
+    val Marie = User("marie@test.fr", "Marie", 22)
+    val Lucie = User("lucie@test.fr", "Lucie", 21)
+    val Matthieu = User("matthieu@test.fr", "Matthieu", 21)
+    Lucie.becomesFriend(Marie)
+    Lucie.becomesFriend(Matthieu)
+    println("Amis de Lucie : ")
+    for(User in Lucie.friends){
+        println(User.name)
+    }
     // Try adding program arguments at Run/Debug configuration
     //println("Program arguments: ${args.joinToString()}")
 }
