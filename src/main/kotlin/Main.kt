0fd2e5e2961c49a8ea5727e3e9f304b9bc8d8133
Lucie Danard <lucie.danard@gmail.com>
2021-10-13 13:46:55 +0200

    change sur Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,21 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
-//    val Marie = User("marie@test.fr", "Marie", 22)
-//    val Lucie = User("lucie@test.fr", "Lucie", 21)
-//    val Matthieu = User("matthieu@test.fr", "Matthieu", 21)
-//    Lucie.becomesFriend(Marie)
-//    Lucie.becomesFriend(Matthieu)
-//    println("Amis de Lucie : ")
-//    for(User in Lucie.friends){
-//        println(User.name)
-//    }
+    val Marie = User("marie@test.fr", "Marie", 22)
+    val Lucie = User("lucie@test.fr", "Lucie", 21)
+    val Matthieu = User("matthieu@test.fr", "Matthieu", 21)
+    Lucie.becomesFriend(Marie)
+    Lucie.becomesFriend(Matthieu)
+    println("Amis de Lucie : ")
+    for(User in Lucie.friends){
+        println(User.name)
+    }
 
-    var m1: Man? = Man()
-    var m2: Man? = m1
-    m1 = null
-    println("m1 : " + m1)
-    println("m2 : " + m2)
+//    var m1: Man? = Man()
+//    var m2: Man? = m1
+//    m1 = null
+//    println("m1 : " + m1)
+//    println("m2 : " + m2)
     // Try adding program arguments at Run/Debug configuration
     //println("Program arguments: ${args.joinToString()}")
 }
